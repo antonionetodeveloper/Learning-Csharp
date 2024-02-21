b3525e79b2f12d862d087a46f10e62181476f4e2
antonionetodeveloper <devantonio.fer@gmail.com>
2024-02-20 23:07:20 -0300

    learning about variables and string properties

---
+++ variables/Program.cs
@@ -0,0 +1,46 @@
+﻿
+byte number1 = 200;
+// from 0 to 255
+// uses 1 byte
+
+sbyte number2 = 120;
+// from -128 to 127
+// uses 1 byte
+
+short number3 = 30000;
+// from -32.767 to 32.766
+// uses 2 bytes
+
+int number4 = 4000000;
+// from -2.147.483.648 to 2.147.483.647
+// uses 4 bytes
+
+long number5 = 500000000000;
+// from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+// uses 8 bytes
+
+
+float number6 = 10.5f;
+// from -3.402823e38 to 3.402823e38
+// uses 4 bytes
+// has 7 digits of precision
+
+double number7 = 10.5;
+// from -1.79769313486232e308 to 1.79769313486232e308
+// uses 8 bytes
+// has 15-16 digits of precision
+
+decimal number8 = 10.5m;
+// from -79228162514264337593543950335 to 79228162514264337593543950335
+// uses 16 bytes
+// has 28-29 digits of precision
+
+
+char character = 'A';
+// uses 2 bytes
+
+string text = "Hello World!";
+// uses 2 bytes per character
+
+bool isTrue = true;
+// uses 1 byte
---
+++ variables/strings/Program.cs
@@ -0,0 +1,11 @@
+﻿string word = "    I like pineapple";
+Console.WriteLine(word);
+
+word = word.Trim();
+Console.WriteLine(word);
+
+string upperLetteredWord = word.ToUpper();
+Console.WriteLine(upperLetteredWord);
+
+string lowerLetterWord = word.ToLower();
+Console.WriteLine(lowerLetterWord);
